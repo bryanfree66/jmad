5bc38c21910e410085c454368f81aa303e4fe643
Bryan Freeman <bryanfree66@gmail.com>
2017-07-20 17:12:05 -0500

    initial index template, view tests

--- solos/views.py
+++ solos/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
 
-def index():
-    pass
+def index(request):
+    return render_to_response('solos/index.html')
