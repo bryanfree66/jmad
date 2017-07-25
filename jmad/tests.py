ae53c12c4a5031beeba0bd386846b2dd66e8a3ef
Bryan Freeman <bryanfree66@gmail.com>
2017-07-25 16:25:37 -0500

    added filtering to index and model data to template

--- jmad/tests.py
+++ jmad/tests.py
@@ -1,5 +1,8 @@
 from django.test import LiveServerTestCase
+
 from selenium import webdriver
+
+from solos.models import Solo
 
 
 class StudentTestCase(LiveServerTestCase):
@@ -8,6 +11,10 @@ class StudentTestCase(LiveServerTestCase):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(2)
 
+        self.solo1 = Solo.objects.create(instrument='saxophone', artist='John Coltrane', track='My Favorite Things')
+        self.solo2 = Solo.objects.create(instrument='saxophone', artist='Cannonball Adderley', track='All Blues')
+        self.solo3 = Solo.objects.create(instrument='saxophone', artist='Cannonball Adderley', track='Waltz for Debby')
+
     def tearDown(self):
         self.browser.quit()
 
@@ -15,49 +22,42 @@ class StudentTestCase(LiveServerTestCase):
         """
         Test that a user can search for solos
         """
-
-        # Steve is a jazz student who would like to find more
-        # examples of solos so he can improve his own
-        # improvisation. He visits the home page of JMAD
+        # Steve is a jazz student who would like to find more examples of solos so he can improve his own improvisation.
+        # He visits the home page of JMAD.
         home_page = self.browser.get(self.live_server_url + '/')
 
-        # he knows he is in the right place because he can see
-        # the name of the site in the heading
+        # He knows he's in the right place because he can see the name of the site in the heading.
         brand_element = self.browser.find_element_by_css_selector('.navbar-brand')
         self.assertEqual('JMAD', brand_element.text)
 
-        # he sees the inputs of the search form, including
-        # labels and placeholders.
+        # He sees the inputs of the search form, including labels and placeholders.
         instrument_input = self.browser.find_element_by_css_selector('input#jmad-instrument')
         self.assertIsNotNone(self.browser.find_element_by_css_selector('label[for="jmad-instrument"]'))
         self.assertEqual(instrument_input.get_attribute('placeholder'), 'i.e. trumpet')
+
         artist_input = self.browser.find_element_by_css_selector('input#jmad-artist')
         self.assertIsNotNone(self.browser.find_element_by_css_selector('label[for="jmad-artist"]'))
         self.assertEqual(artist_input.get_attribute('placeholder'), 'i.e. Davis')
 
-        # He types in the name of his instrument and submits
-        # it
+        # He types in the name of his instrument and submits it.
         instrument_input.send_keys('saxophone')
         self.browser.find_element_by_css_selector('form button').click()
 
         # He sees too many search results...
-        # ... so he adds an artist to his search query and
-        # gets a more manageable list
         search_results = self.browser.find_elements_by_css_selector('.jmad-search-result')
-        self.assertGreater(len(search_results),2)
+        self.assertGreater(len(search_results), 2)
 
+        # ...so he adds an artist to his search query and gets a more manageable list.
         second_artist_input = self.browser.find_element_by_css_selector('input#jmad-artist')
         second_artist_input.send_keys('Cannonball Adderley')
         self.browser.find_element_by_css_selector('form button').click()
-        second_search_result = self.browser.find_elements_by_css_selector('.jmad-search-result')
-        self.assertEqual(len(second_search_result),2)
 
+        second_search_results = self.browser.find_elements_by_css_selector('.jmad-search-result')
+        self.assertEqual(len(second_search_results), 2)
         self.fail('Incomplete Test')
 
-        # He clicks on a search result
+        # He clicks on a search result.
 
-        # the solo page has an title, artist, and album for
-        # this particular solo
+        # The solo page has the title, artist and album for this particular solo.
 
-        # He also sees the start time and end time of the
-        # solo
+        # He also sees the start time and end time of the solo.
